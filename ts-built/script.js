160d15c9d6cf69f4e33d0bcc6104aaad0a1ea599
Melanie Anjos <a.melanie993@gmail.com>
2024-03-01 12:26:40 +0100

    converted into typescript

---
+++ ts-built/script.js
@@ -0,0 +1,16 @@
+var canvas;
+var ctx;
+var background = new Image();
+background.src = "https://flying-pikachu.com/assets/img/intro/banner_flying-pikachu_en.webp";
+function createCanvas() {
+    var canvas2 = document.createElement('canvas');
+    ctx = canvas2.getContext("2d");
+    canvas2.width = 671;
+    canvas2.height = 475;
+    document.body.appendChild(canvas2);
+    ctx.drawImage(background, 0, 0);
+}
+// Make sure the image is loaded first otherwise nothing will draw.
+background.onload = function () {
+    createCanvas();
+};
