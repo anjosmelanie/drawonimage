6e2ac5e69f6bccd711d49ea1921ee33862f5e5fd
Melanie Anjos <a.melanie993@gmail.com>
2024-03-02 14:09:30 +0100

    path wip

--- script/script.ts
+++ script/script.ts
@@ -10,13 +10,13 @@ background.onload = function(){
 }
 
 function createCanvas(){
-    const canvas = document.createElement('canvas');
-    ctx = canvas.getContext("2d");
+    this.canvas = document.createElement('canvas');
+    this.ctx = canvas.getContext("2d");
     canvas.width = 671;
     canvas.height = 475;
     canvas.id = "canvas-1"
     document.body.appendChild(canvas);
-    ctx!.drawImage(background,0,0);   
+    this.ctx.drawImage(background,0,0);   
 }
 
 function drawRectangle(){
@@ -27,4 +27,63 @@ function drawRectangle(){
       ctx.clearRect(45, 45, 60, 60);
       ctx.strokeRect(50, 50, 50, 50);
     }
+}
+
+function drawPath(){
+    const canvas = <HTMLCanvasElement>document.getElementById("canvas-1");
+    if (canvas.getContext) {
+      const ctx = canvas.getContext("2d");
+      ctx.fillStyle = "rgb(255 165 0 / 10%)";
+      ctx.beginPath();
+      ctx.moveTo(275, 50); 
+      ctx.lineTo(300, 75);
+      ctx.lineTo(400, 25);
+      ctx.lineTo(275, 50);
+      ctx.fill(); //fill: filled, stroke: only outline
+      
+    }
+}
+
+function drawMyPath(){
+    const canvas = <HTMLCanvasElement>document.getElementById("canvas-1");
+    var coordinates = [];
+    document.addEventListener("click", getCoordinatesOnClick.bind(coordinates));
+    console.log(coordinates); //TODO: Remove
+    if (canvas.getContext) {
+        var init = false;
+        const ctx = canvas.getContext("2d");
+        this.ctx.beginPath();
+        ctx.moveTo(coordinates[1].x, coordinates[1].y);
+        ctx.lineTo(coordinates[2].x, coordinates[2].y);
+        ctx.lineTo(coordinates[3].x, coordinates[3].y);
+        ctx.lineTo(coordinates[4].x, coordinates[4].y);
+        ctx.lineTo(coordinates[1].x, coordinates[1].y);
+        ctx.stroke(); //fill: filled, stroke: only outline
+    }
+}
+
+
+//var coords = [];
+
+/* 
+moveTo(x, y)
+Moves the pen to the coordinates specified by x and y.
+
+lineTo(x, y)
+Draws a line from the current drawing position to the position specified by x and y.
+
+arc(x, y, radius, startAngle, endAngle, counterclockwise)
+Draws an arc which is centered at (x, y) position with radius r starting at startAngle and ending at endAngle going in the given direction indicated by counterclockwise (defaulting to clockwise).
+
+arcTo(x1, y1, x2, y2, radius)
+Draws an arc with the given control points and radius, connected to the previous point by a straight line. */
+
+function storeCoordinate(xVal, yVal, array){
+    array.push({"x": xVal, "y": yVal});
+    //TODO: Aqui la logica de cuando se cierra el array?
+}
+
+function getCoordinatesOnClick(event){
+    console.log("Mouse: " + event.clientX, ", " + event.clientY);
+    storeCoordinate(event.clientX, event.clientY, this); //this es el array de coordenadas que esta bound
 }
--- ts-built/script.js
+++ ts-built/script.js
@@ -7,13 +7,13 @@ background.onload = function () {
     createCanvas();
 };
 function createCanvas() {
-    var canvas = document.createElement('canvas');
-    ctx = canvas.getContext("2d");
+    this.canvas = document.createElement('canvas');
+    this.ctx = canvas.getContext("2d");
     canvas.width = 671;
     canvas.height = 475;
     canvas.id = "canvas-1";
     document.body.appendChild(canvas);
-    ctx.drawImage(background, 0, 0);
+    this.ctx.drawImage(background, 0, 0);
 }
 function drawRectangle() {
     var canvas = document.getElementById("canvas-1");
@@ -23,4 +23,55 @@ function drawRectangle() {
         ctx_1.clearRect(45, 45, 60, 60);
         ctx_1.strokeRect(50, 50, 50, 50);
     }
+}
+function drawPath() {
+    var canvas = document.getElementById("canvas-1");
+    if (canvas.getContext) {
+        var ctx_2 = canvas.getContext("2d");
+        ctx_2.fillStyle = "rgb(255 165 0 / 10%)";
+        ctx_2.beginPath();
+        ctx_2.moveTo(275, 50);
+        ctx_2.lineTo(300, 75);
+        ctx_2.lineTo(400, 25);
+        ctx_2.lineTo(275, 50);
+        ctx_2.fill(); //fill: filled, stroke: only outline
+    }
+}
+function drawMyPath() {
+    var canvas = document.getElementById("canvas-1");
+    var coordinates = [];
+    document.addEventListener("click", getCoordinatesOnClick.bind(coordinates));
+    console.log(coordinates); //TODO: Remove
+    if (canvas.getContext) {
+        var init = false;
+        var ctx_3 = canvas.getContext("2d");
+        this.ctx.beginPath();
+        ctx_3.moveTo(coordinates[1].x, coordinates[1].y);
+        ctx_3.lineTo(coordinates[2].x, coordinates[2].y);
+        ctx_3.lineTo(coordinates[3].x, coordinates[3].y);
+        ctx_3.lineTo(coordinates[4].x, coordinates[4].y);
+        ctx_3.lineTo(coordinates[1].x, coordinates[1].y);
+        ctx_3.stroke(); //fill: filled, stroke: only outline
+    }
+}
+//var coords = [];
+/*
+moveTo(x, y)
+Moves the pen to the coordinates specified by x and y.
+
+lineTo(x, y)
+Draws a line from the current drawing position to the position specified by x and y.
+
+arc(x, y, radius, startAngle, endAngle, counterclockwise)
+Draws an arc which is centered at (x, y) position with radius r starting at startAngle and ending at endAngle going in the given direction indicated by counterclockwise (defaulting to clockwise).
+
+arcTo(x1, y1, x2, y2, radius)
+Draws an arc with the given control points and radius, connected to the previous point by a straight line. */
+function storeCoordinate(xVal, yVal, array) {
+    array.push({ "x": xVal, "y": yVal });
+    //TODO: Aqui la logica de cuando se cierra el array?
+}
+function getCoordinatesOnClick(event) {
+    console.log("Mouse: " + event.clientX, ", " + event.clientY);
+    storeCoordinate(event.clientX, event.clientY, this); //this es el array de coordenadas que esta bound
 }
