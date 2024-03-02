f7c357f2382fdbe1e887d35bcca602f7ffe6f336
Melanie Anjos <a.melanie993@gmail.com>
2024-03-02 12:21:51 +0100

    Add buttons and draw rectangle test

--- script/script.ts
+++ script/script.ts
@@ -4,16 +4,27 @@ var ctx: CanvasRenderingContext2D | null;
 var background = new Image();
 background.src = "https://flying-pikachu.com/assets/img/intro/banner_flying-pikachu_en.webp";
 
-function createCanvas(){
-    var canvas2 = document.createElement('canvas');
-    ctx = canvas2.getContext("2d");
-    canvas2.width = 671;
-    canvas2.height = 475;
-    document.body.appendChild(canvas2);
-    ctx!.drawImage(background,0,0);   
-}
-
 // Make sure the image is loaded first otherwise nothing will draw.
 background.onload = function(){
     createCanvas();
+}
+
+function createCanvas(){
+    const canvas = document.createElement('canvas');
+    ctx = canvas.getContext("2d");
+    canvas.width = 671;
+    canvas.height = 475;
+    canvas.id = "canvas-1"
+    document.body.appendChild(canvas);
+    ctx!.drawImage(background,0,0);   
+}
+
+function drawRectangle(){
+    const canvas = <HTMLCanvasElement>document.getElementById("canvas-1");
+    if (canvas.getContext) {
+      const ctx = canvas.getContext("2d");
+      ctx.fillRect(25, 25, 100, 100);
+      ctx.clearRect(45, 45, 60, 60);
+      ctx.strokeRect(50, 50, 50, 50);
+    }
 }
--- ts-built/script.js
+++ ts-built/script.js
@@ -2,15 +2,25 @@ var canvas;
 var ctx;
 var background = new Image();
 background.src = "https://flying-pikachu.com/assets/img/intro/banner_flying-pikachu_en.webp";
-function createCanvas() {
-    var canvas2 = document.createElement('canvas');
-    ctx = canvas2.getContext("2d");
-    canvas2.width = 671;
-    canvas2.height = 475;
-    document.body.appendChild(canvas2);
-    ctx.drawImage(background, 0, 0);
-}
 // Make sure the image is loaded first otherwise nothing will draw.
 background.onload = function () {
     createCanvas();
 };
+function createCanvas() {
+    var canvas = document.createElement('canvas');
+    ctx = canvas.getContext("2d");
+    canvas.width = 671;
+    canvas.height = 475;
+    canvas.id = "canvas-1";
+    document.body.appendChild(canvas);
+    ctx.drawImage(background, 0, 0);
+}
+function drawRectangle() {
+    var canvas = document.getElementById("canvas-1");
+    if (canvas.getContext) {
+        var ctx_1 = canvas.getContext("2d");
+        ctx_1.fillRect(25, 25, 100, 100);
+        ctx_1.clearRect(45, 45, 60, 60);
+        ctx_1.strokeRect(50, 50, 50, 50);
+    }
+}
